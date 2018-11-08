8ac66327c1c237a07b20bbe82eb1c5d8eaffc5dc
oeloem <rosihululum2@gmail.com>
2018-11-08 14:35:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/oeloem/comversibilangan/Hexa.java
@@ -0,0 +1,41 @@
+package com.example.oeloem.comversibilangan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Hexa extends AppCompatActivity {
+    EditText desi,bin;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hexa);
+        desi = (EditText)findViewById(R.id.desimal);
+        bin = (EditText)findViewById(R.id.hexa);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int inputdesimal = Integer.parseInt(desi.getText().toString());
+                int i=0, hasil =0;
+                int []biner = new int[9];
+                String hasilbiner="";
+                i = 9;
+                while (inputdesimal > 0){
+                    hasil = inputdesimal %8;
+                    inputdesimal= (inputdesimal - hasil)/8;
+                    i--;
+                    biner [i]= hasil;
+                    hasilbiner = biner[i]+ hasilbiner;
+
+                }
+                bin.setText(hasilbiner);
+            }
+        });
+    }
+}
+
+
